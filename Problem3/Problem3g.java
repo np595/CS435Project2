46c3e4fa390b4e1d2ecf83dfa0c472a8b6b6b6fe
np595 <49132942+np595@users.noreply.github.com>
2020-03-30 12:04:44 -0700

    Update Problem3g.java

--- Problem3/Problem3g.java
+++ Problem3/Problem3g.java
@@ -78,5 +78,7 @@ class Main{
                 Random rand = new Random();
                 Graph graphing = new Graph();
                 Main test = new Main();
+                
+                graphing = test.createRandomUnweightedGraphIter(nodes, nodeSize);
         }
 }

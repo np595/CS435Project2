2559da3e0d5e6c1a1b95f6d9d2c8c708dda7f430
np595 <49132942+np595@users.noreply.github.com>
2020-04-04 13:09:30 -0700

    Create Problem4c.java

---
+++ Problem4/Problem4c.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.util.Random;
+
+class Edge{
+        public Node dest;
+        public String weight;
+        public Edge(Node d, String w){
+                this.dest = d;
+                this.weight = w;
+        }
+}
+
+class Node{
+        public String data;
+        public List<Edge> neighbors;
+        public Node(String d){
+                this.data = d;
+        }
+}
+
+class MazeGraph{
+        List<Node> nodes;
+}
+
+class DirectedGraph{
+        HashSet<Node> graph = new HashSet<Node>();
+        public int n = 0;
+
+        Node addNode(final String nodeVal){
+                Node temp = new Node(nodeVal);
+                temp.neighbors = new ArrayList<Edge>();
+                graph.add(temp);
+                n++;
+                return temp;
+        }
+
+        void addDirectedEdge(final Node first, final Node second){
+                first.neighbors.add(new Edge(second, second.data));
+        }
+
+        void removeDirectedEdge(final Node first, final Node second){
+                boolean check = false;
+                for(int i = 0; i < first.neighbors.size(); i++){
+                        if(first.neighbors.get(i).dest == second){
+                                first.neighbors.remove(i);
+                                check = true;
+                                break;
+                        }
+                }
+                if(check == false){
+                        System.out.println(first.data + " isn't connected to " + second.data);
+                }
+        }
+
+        HashSet<Node> getAllNodes(){
+                HashSet<Node> allNode = graph;
+                return allNode;
+        }
+}
+
+class Main{
+        DirectedGraph createRandomDAGIter(Node[] nodes, final int n){
+                Random rand = new Random();
+                DirectedGraph graphing = new DirectedGraph();
+                String alp = "abcdefghijklmnopqrstuvxyz";
+                int size = alp.length();
+                for(int i = 0; i < n; i++){
+                        char tempS = alp.charAt(rand.nextInt(size));
+                        String temp = String.valueOf(tempS);
+                        nodes[i] = graphing.addNode(temp);
+                }
+                for(int k = 0; k < (n*2); k++){
+                        int nm = rand.nextInt(nodes.length);
+                        int m = rand.nextInt(nodes.length);
+                        graphing.addDirectedEdge(nodes[nm], nodes[m]);
+                }
+                System.out.println(nodes[0].data + " " + nodes[0].neighbors);
+                return graphing;
+        }
+        
+        public static void main(String[] args){
+                int nodeSize = 5;
+                Node[] nodes = new Node[nodeSize];
+                Random rand = new Random();
+                DirectedGraph graphing = new DirectedGraph();
+                Main test = new Main();
+
+                graphing = test.createRandomDAGIter(nodes, nodeSize);
+        }
+}

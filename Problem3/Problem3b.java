f6787e57b85b88f5e0c50dc8dcf96ccbbd454a3d
np595 <49132942+np595@users.noreply.github.com>
2020-04-19 13:45:58 -0700

    Update Problem3b.java

--- Problem3/Problem3b.java
+++ Problem3/Problem3b.java
@@ -75,7 +75,7 @@ class Main{
         
         Graph createRandomUnweightedGraphIter(Node[] nodes, int n){
                 Random rand = new Random();
-                Graph graphing = new Graph();
+                Graph undirGraph = new Graph();
                 //String alp = "abcdefghijklmnopqrstuvxyz";
                 //int size = alp.length();
                 for(int i = 0; i < n; i++){
@@ -83,23 +83,23 @@ class Main{
                         //String temp = String.valueOf(tempS);
                         int tempS = rand.nextInt(1000);
                         String temp = Integer.toString(tempS);
-                        nodes[i] = graphing.addNode(temp);
+                        nodes[i] = undirGraph.addNode(temp);
                 }
                 for(int k = 0; k < n; k++){
                         int firstNode = rand.nextInt(nodes.length);
                         int secondNode = rand.nextInt(nodes.length);
-                        graphing.addUndirectedEdge(nodes[firstNode], nodes[secondNode]);
+                        undirGraph.addUndirectedEdge(nodes[firstNode], nodes[secondNode]);
                 }
-                return graphing;
+                return undirGraph;
         }
         
         public static void main(String[] args){
                 int nodeSize = 5;
                 Node[] nodes = new Node[nodeSize];
                 Random rand = new Random();
-                Graph graphing = new Graph();
+                Graph undirGraph = new Graph();
                 Main test = new Main();
                 
-                graphing = test.createRandomUnweightedGraphIter(nodes, nodeSize);
+                undirGraph = test.createRandomUnweightedGraphIter(nodes, nodeSize);
         }
 }

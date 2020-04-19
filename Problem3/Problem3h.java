4f91cb11490e6780411954a9783e4e823a739882
np595 <49132942+np595@users.noreply.github.com>
2020-04-19 13:53:08 -0700

    Update Problem3h.java

--- Problem3/Problem3h.java
+++ Problem3/Problem3h.java
@@ -106,34 +106,36 @@ class Main{
         
         Graph createLinkedList(Node[] nodes, int n){
                 Random rand = new Random();
-                Graph graphing = new Graph();
-                String alp = "abcdefghijklmnopqrstuvxyz";
-                int size = alp.length();
+                Graph undirGraph = new Graph();
+                //String alp = "abcdefghijklmnopqrstuvxyz";
+                //int size = alp.length();
                 for(int i = 0; i < n; i++){
-                        char tempS = alp.charAt(rand.nextInt(size));
-                        String temp = String.valueOf(tempS);
-                        nodes[i] = graphing.addNode(temp);
+                        //char tempS = alp.charAt(rand.nextInt(size));
+                        //String temp = String.valueOf(tempS);
+                        int tempS = rand.nextInt(1000);
+                        String temp = Integer.toString(tempS);
+                        nodes[i] = undirGraph.addNode(temp);
                 }
-                for(int k = 0; k < nodes.length; k++){
-                        int nm = k;
-                        int m = k + 1;
-                        if(m < nodes.length){
-                                graphing.addUndirectedEdge(nodes[nm], nodes[m]);
+                for(int k = 0; k < n; k++){
+                        int firstNode = k;
+                        int secondNode = k + 1;
+                        if(secondNode < nodes.length){
+                                undirGraph.addUndirectedEdge(nodes[firstNode], nodes[secondNode]);
                         }
                 }
                 ArrayList<Node> check = new ArrayList<Node>();
-                check = BFT(graphing);
+                check = BFT(undirGraph);
 
-                return graphing;
+                return undirGraph;
         }
         
         public static void main(String[] args){
                 int nodeSize = 10000;
                 Node[] nodes = new Node[nodeSize];
                 Random rand = new Random();
-                Graph graphing = new Graph();
+                Graph undirGraph = new Graph();
                 Main test = new Main();
                 
-                graphing = test.createLinkedList(nodes, nodeSize);
+                undirGraph = test.createLinkedList(nodes, nodeSize);
         }
 }

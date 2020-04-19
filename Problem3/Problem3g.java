c59b40ad1e912a9a5486434d83757e08351dc244
np595 <49132942+np595@users.noreply.github.com>
2020-04-19 13:14:40 -0700

    Update Problem3g.java

--- Problem3/Problem3g.java
+++ Problem3/Problem3g.java
@@ -101,17 +101,19 @@ class Main{
         Graph createRandomUnweightedGraphIter(Node[] nodes, int n){
                 Random rand = new Random();
                 Graph graphing = new Graph();
-                String alp = "abcdefghijklmnopqrstuvxyz";
-                int size = alp.length();
+                //String alp = "abcdefghijklmnopqrstuvxyz";
+                //int size = alp.length();
                 for(int i = 0; i < n; i++){
-                        char tempS = alp.charAt(rand.nextInt(size));
-                        String temp = String.valueOf(tempS);
+                        //char tempS = alp.charAt(rand.nextInt(size));
+                        //String temp = String.valueOf(tempS);
+                        int tempS = rand.nextInt(1000);
+                        String temp = Integer.toString(tempS);
                         nodes[i] = graphing.addNode(temp);
                 }
                 for(int k = 0; k < n; k++){
-                        int nm = rand.nextInt(nodes.length);
-                        int m = rand.nextInt(nodes.length);
-                        graphing.addUndirectedEdge(nodes[nm], nodes[m]);
+                        int firstNode = rand.nextInt(nodes.length);
+                        int secondNode = rand.nextInt(nodes.length);
+                        graphing.addUndirectedEdge(nodes[firstNode], nodes[secondNode]);
                 }
 
                 ArrayList<Node> check = new ArrayList<Node>();

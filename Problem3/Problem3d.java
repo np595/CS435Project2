5aa626eb47650777320bae61ed63a33c8ed9a15e
np595 <49132942+np595@users.noreply.github.com>
2020-04-19 13:26:15 -0700

    Update Problem3d.java

--- Problem3/Problem3d.java
+++ Problem3/Problem3d.java
@@ -72,8 +72,6 @@ class Graph{
 }
 
 class Main{
-        
-        int q = 0;
 
         ArrayList<Node> DFSRec(final Node start, final Node end, int size, ArrayList<Node> visited){
                 ArrayList<Node> check = new ArrayList<Node>();
@@ -81,7 +79,6 @@ class Main{
                 for(int j = 0; j < start.neighbors.size(); j++){
                         if(start.data != end.data){
                                 if(!visited.contains(start.neighbors.get(j).dest)){
-                                        q++;
                                         check = DFSRec(start.neighbors.get(j).dest, end, size, visited);
                                 }
                         }
@@ -106,17 +103,19 @@ class Main{
         Graph createRandomUnweightedGraphIter(Node[] nodes, int n){
                 Random rand = new Random();
                 Graph graphing = new Graph();
-                String alp = "abcdefghijklmnopqrstuvxyz";
-                int size = alp.length();
+                //String alp = "abcdefghijklmnopqrstuvxyz";
+                //int size = alp.length();
                 for(int i = 0; i < n; i++){
-                        char tempS = alp.charAt(rand.nextInt(size));
-                        String temp = String.valueOf(tempS);
+                        //char tempS = alp.charAt(rand.nextInt(size));
+                        //String temp = String.valueOf(tempS);
+                        int tempS = rand.nextInt(1000);
+                        String temp = Integer.toString(tempS);
                         nodes[i] = graphing.addNode(temp);
                 }
                 for(int k = 0; k < n; k++){
-                        int nm = rand.nextInt(nodes.length);
-                        int m = rand.nextInt(nodes.length);
-                        graphing.addUndirectedEdge(nodes[nm], nodes[m]);
+                        int firstNode = rand.nextInt(nodes.length);
+                        int secondNode = rand.nextInt(nodes.length);
+                        graphing.addUndirectedEdge(nodes[firstNode], nodes[secondNode]);
                 }
 
                 ArrayList<Node> check = new ArrayList<Node>();
